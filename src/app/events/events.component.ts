907ac678b09fb58a1e8f31ba2ce6f0c5e95c5beb
Luis Velásquez <lgvv06@gmail.com>
2020-03-03 22:56:52 -0600

    Small import redundancy fix

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { EventService } from '@gameStateSync/core-data';
-import { Event, EventStatistics, EventsFacade } from '@gameStateSync/core-data';
+import { Event, EventStatistics, EventsFacade, EventService } from '@gameStateSync/core-data';
 import { Observable } from 'rxjs';
 
 @Component({
